0340de8f462546f02660a43a502922a5a34813f1
Abhishek Shankar <abhishek.201904069@tulas.edu.in>
2024-08-11 03:20:14 +0530

    Api for category integrated

--- src/app/admin/content/newCategory/page.tsx
+++ src/app/admin/content/newCategory/page.tsx
@@ -1,15 +1,88 @@
-import React from 'react';
-import { FiUser } from 'react-icons/fi';
+'use client';
+
+
+import React, { useState } from 'react';
+import { FiUser, FiFilePlus, FiLoader } from 'react-icons/fi';
 import { AiOutlineUserAdd } from 'react-icons/ai'; // Importing a new icon for adding contact
+import { Alert, Spinner } from 'react-bootstrap';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const NewCategoryPage: React.FC = () => {
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
+    const [contentCategoryImage, setContentCategoryImage] = useState('');
+    console.log(contentCategoryImage)
+    const [loading, setLoading] = useState<boolean>(false);
+
+
+    const handleImageUpload = (e: any) => {
+        setContentCategoryImage(e.target.files[0]);
+    };
+    const showToast = (message: string) => {
+        toast.error(message, {
+            position: "top-center",
+            autoClose: 3000, // Auto dismiss after 3 seconds
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+
+    const handleSubmit = async () => {
+        const missingFields = [];
+        if (!category) missingFields.push('Category');
+        if (!description) missingFields.push('Description');
+        if (!contentCategoryImage) missingFields.push('Image');
+
+        if (missingFields.length > 0) {
+            showToast(`Please fill in the following fields: ${missingFields.join(', ')}`);
+            return;
+        }
+        setLoading(true);
+        const formData = new FormData();
+        console.log("clicked")
+        formData.append("category", category);
+        formData.append("description", description);
+        formData.append("contentCategoryImage", contentCategoryImage);
+
+        try {
+            console.log("entered")
+            const response = await fetch('https://harmony-backend-z69j.onrender.com/api/admin/create/content/category', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const result = await response.json();
+            console.log("result", result);
+            const firstWord = result.message.split(' ')[0];
+
+
+            showToast('Category Created Successfully');
+
+            setCategory('');
+            setDescription('');
+            setContentCategoryImage('');
+            setLoading(false);
+
+
+        } catch (error) {
+            console.error("Error:", error);
+            setLoading(false);
+        }
+    };
     return (
-        <div style={styles.formContainer}>
+        <>
+            <div style={styles.formContainer}>
                 <div style={styles.row}>
                     <div style={styles.inputGroup}>
                         <input
                             type="text"
-                            placeholder="Name"
+                            placeholder="Category"
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
                             style={styles.input}
                         />
                         <div>
@@ -17,45 +90,67 @@ const NewCategoryPage: React.FC = () => {
                         </div>
                     </div>
                     <div style={styles.inputGroup}>
-                        <input
-                            type="upload"
-                            placeholder="Upload Cover Photo"
-                            style={styles.input}
-                        />
+                        <label style={{ marginLeft: 10, display: 'flex', alignItems: 'center', justifyContent: 'space-between', width:'100%' }} >
+                            <input
+                                type="file"
+                                onChange={handleImageUpload}
+                                style={{ display: 'none' }}
+                            />
+
+                            {contentCategoryImage === '' ? 'Choose a file' : contentCategoryImage?.name}
+                            <div>
+                                <FiFilePlus style={styles.icon} />
+
+                            </div>
+
+                        </label>
+
+
                     </div>
                 </div>
-                <div style={{width: "50%",
-                             height: '300px',
-                             backgroundColor: "#fff",
-                             padding: "10px",
-                             borderRadius: "10px",
-                             border: '1px solid #ddd',
-                             display: "flex",
-                             justifyContent: "space-between",
-                             resize: 'none' as const,}}>
+                <div style={{
+                    width: "100%",
+                    height: '300px',
+                    backgroundColor: "#fff",
+                    padding: "10px",
+                    borderRadius: "10px",
+                    border: '1px solid #ddd',
+                    display: "flex",
+                    justifyContent: "space-between",
+                    resize: 'none'
+                }}>
                     <textarea
                         placeholder="Description"
-                        style={{ ...styles.input}}
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        style={styles.input}
                     />
                     <div className='flex-end'>
                         <FiUser style={styles.icon} />
                     </div>
                 </div>
-                <button style={styles.saveButton}>
-                    Save <AiOutlineUserAdd style={styles.buttonIcon} />
+                <button style={styles.saveButton} onClick={handleSubmit}>
+                    {!loading ?
+                        <div style={{ display: 'flex' }}>Save <AiOutlineUserAdd style={{ ...styles.buttonIcon, fontSize: '1.5rem' }} /></div> : <Spinner className='' animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    }
                 </button>
+
             </div>
+            <ToastContainer />
+        </>
     );
 }
 
 const styles = {
-   
+
     formContainer: {
         backgroundColor: '#fff',
         borderRadius: '15px',
         margin: "20px 25px",
         padding: '20px',
-        width: '96%',
+        width: '56%',
         // maxWidth: '1200px', // Increase the max-width for the container
         // boxSizing: 'border-box' as const,
         display: 'flex',
@@ -67,7 +162,7 @@ const styles = {
         display: 'flex',
         justifyContent: 'space-between',
         gap: "40px",
-        width: '50%',
+        width: '100%',
         marginBottom: '20px',
     },
     inputGroup: {
@@ -89,8 +184,8 @@ const styles = {
         outline: 'none',
         backgroundColor: 'transparent', // Ensure the input blends with the container
     },
-  
-  
+
+
     icon: {
         color: '#ff8a00',
         fontSize: '2rem',
@@ -99,17 +194,18 @@ const styles = {
     saveButton: {
         backgroundColor: '#ff8a00',
         color: '#fff',
-        padding: '10px 20px',
+
         borderRadius: '10px',
         border: 'none',
         cursor: 'pointer',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         fontSize: '1rem',
         marginTop: '20px',
-        alignSelf: 'center' as const, // Center the button
-        width: '150px',
+        alignSelf: 'center', // Center the button
+        width: '100px',
+        height: '50px'
     },
     buttonIcon: {
         marginLeft: '10px',
