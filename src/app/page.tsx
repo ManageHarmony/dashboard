efc13589a1836859b3c29df13dc56cc3c1f4c3a3
abhishekshanka28 <abhishekshankar28@gmail.com>
2024-08-15 22:15:29 +0530

    home page navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-
-
-
-
+import Link from "next/link";
+import { Button } from "react-bootstrap";
 
 
 export default function Home() {
   return (
-   <>
-    <h1>Hello</h1>
-   </>
+    <>
+      <div style={{width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{ width: "80%",display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
+          <Link href='/admin'><Button>Go to Admin Page</Button></Link>
+          <Link href='/manager'><Button>Go to manager Page</Button></Link>
+          <Link href='/creator'><Button>Go to creator Page</Button></Link>
+        </div>
+      </div>
+    </>
   );
 }
