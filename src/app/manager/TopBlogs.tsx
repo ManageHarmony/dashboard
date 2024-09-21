7b373f4e196ce4ac86680f3035953f1119e5d3e4
abhishekshanka28 <abhishekshankar28@gmail.com>
2024-09-21 23:20:56 +0530

    chnages are goin on

--- src/app/manager/TopBlogs.tsx
+++ src/app/manager/TopBlogs.tsx
@@ -30,6 +30,12 @@ const TopBlogs = () => {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  if (!apiKey) {
+    throw new Error('API key is missing.');
+  }
+
   useEffect(() => {
     const fetchContent = async () => {
       setLoading(true);
@@ -44,7 +50,10 @@ const TopBlogs = () => {
           const managerUsername = decodedToken.username;
           console.log("manager", managerUsername)
 
-          const response = await fetch(`https://harmony-backend-z69j.onrender.com/api/manager/get/content?managerUsername=${managerUsername}`);
+          const response = await fetch(`https://harmony-backend-z69j.onrender.com/api/manager/get/content?managerUsername=${managerUsername}`, {
+            method: "GET", headers: { 'x-api-key': apiKey }
+
+          });
           const data = await response.json();
           console.log("data", data);
 
@@ -66,7 +75,7 @@ const TopBlogs = () => {
 
   const handleClick = (id: number) => {
     router.push(`/manager/content/allblogs/${id}`)
-}
+  }
 
   const displayedData = topBlogs.slice(0, 5);
 
@@ -81,7 +90,7 @@ const TopBlogs = () => {
         </CardTitle>
         <Link href="/manager/allBlogs" style={{ textDecoration: "none" }}>
           <button
-         className='see-all'
+            className='see-all'
           >
             See All
             <svg xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: "5px" }} width="16" height="17" viewBox="0 0 16 17" fill="none">
@@ -96,8 +105,8 @@ const TopBlogs = () => {
       <div style={{ maxHeight: "calc(100% - 20px)", overflowY: "auto" }} className="scrollable-content">
         {loading ? (
           <div className="text-center mt-5">
-          <p className="text-gray-600">Loading Blogs...</p>
-        </div>
+            <p className="text-gray-600">Loading Blogs...</p>
+          </div>
         ) : (
           <table className="table-auto w-full border-collapse">
             <thead className="bg-orange-100 rounded-t-lg">
@@ -115,10 +124,10 @@ const TopBlogs = () => {
                   <td className="p-2 text-black">{blog?.data?.headings.h1[0]}</td>
                   <td className="p-2 text-black">{blog?.blog_creatorId}</td>
                   <td className="p-2 text-black cursor-pointer" onClick={() => handleClick(blog.id)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                        <rect width="32" height="32" rx="6" fill="#FFE3D0" />
-                        <path d="M29.0947 16.5733C28.984 16.4147 26.3454 12.648 22.704 10.0733C20.816 8.736 18.4347 8 16 8C13.5667 8 11.1854 8.736 9.29336 10.0733C5.65203 12.648 3.01603 16.4147 2.90536 16.5733C2.58803 17.0307 2.58803 17.6373 2.90536 18.0947C3.01603 18.2533 5.65203 22.02 9.29336 24.5947C11.1854 25.9307 13.5667 26.6667 16 26.6667C18.4347 26.6667 20.816 25.9307 22.704 24.5933C26.3454 22.0187 28.984 18.252 29.0947 18.0933C29.4134 17.6373 29.4134 17.0293 29.0947 16.5733ZM16 22C13.4214 22 11.3334 19.9067 11.3334 17.3333C11.3334 14.7547 13.4214 12.6667 16 12.6667C18.5734 12.6667 20.6667 14.7547 20.6667 17.3333C20.6667 19.9067 18.5734 22 16 22ZM18.6667 17.3333C18.6667 18.8027 17.4694 20 16 20C14.5267 20 13.3334 18.8027 13.3334 17.3333C13.3334 15.86 14.5267 14.6667 16 14.6667C17.4694 14.6667 18.6667 15.86 18.6667 17.3333Z" fill="#FFA05D" />
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                      <rect width="32" height="32" rx="6" fill="#FFE3D0" />
+                      <path d="M29.0947 16.5733C28.984 16.4147 26.3454 12.648 22.704 10.0733C20.816 8.736 18.4347 8 16 8C13.5667 8 11.1854 8.736 9.29336 10.0733C5.65203 12.648 3.01603 16.4147 2.90536 16.5733C2.58803 17.0307 2.58803 17.6373 2.90536 18.0947C3.01603 18.2533 5.65203 22.02 9.29336 24.5947C11.1854 25.9307 13.5667 26.6667 16 26.6667C18.4347 26.6667 20.816 25.9307 22.704 24.5933C26.3454 22.0187 28.984 18.252 29.0947 18.0933C29.4134 17.6373 29.4134 17.0293 29.0947 16.5733ZM16 22C13.4214 22 11.3334 19.9067 11.3334 17.3333C11.3334 14.7547 13.4214 12.6667 16 12.6667C18.5734 12.6667 20.6667 14.7547 20.6667 17.3333C20.6667 19.9067 18.5734 22 16 22ZM18.6667 17.3333C18.6667 18.8027 17.4694 20 16 20C14.5267 20 13.3334 18.8027 13.3334 17.3333C13.3334 15.86 14.5267 14.6667 16 14.6667C17.4694 14.6667 18.6667 15.86 18.6667 17.3333Z" fill="#FFA05D" />
+                    </svg>
                   </td>
                 </tr>
               ))}
