838515c0c1f676813748e89fb31208e709b1c573
abhishekshanka28 <abhishekshankar28@gmail.com>
2024-09-24 12:44:14 +0530

    all work done yet

---
+++ src/app/consultants/page.tsx
@@ -0,0 +1,211 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Grid, Button } from '@mui/material';
+import StaffCard from '@/components/StaffCard';
+import Link from 'next/link';
+import { Spinner } from 'react-bootstrap';
+import './consultant.css'
+type RoleType = 'All' | 'Approved' | 'Pending' | 'Rejected';
+
+
+
+const buttonStyle = {
+    background: "#fff",
+    color: "#2C297E",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    transition: "background-color 0.3s, color 0.3s, border 0.3s"
+};
+
+const buttonHoverStyle = {
+    background: "#2C297E",
+    color: "white"
+};
+
+const StaffCardView: React.FC = () => {
+    const [hoveredButton, setHoveredButton] = useState<string | null>(null);
+    const [doctorData, setDoctorData] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [selected, setSelected] = useState('All');
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+    if (!apiKey) {
+        throw new Error('API key is missing.');
+    }
+
+    // Fetch doctors based on role
+    const fetchDoctors = async (role: RoleType) => {
+        setLoading(true);
+        const urlMap: Record<RoleType, string> = {
+            All: 'https://harmony-backend-z69j.onrender.com/api/admin/get/staff',
+            Approved: 'https://harmony-backend-z69j.onrender.com/api/admin/getApprovedDoctors',
+            Pending: 'https://harmony-backend-z69j.onrender.com/api/admin/getPendingDoctors',
+            Rejected: 'https://harmony-backend-z69j.onrender.com/api/admin/get/rejectedDoctors',
+            // Active: 'https://harmony-backend-z69j.onrender.com/api/admin/get/active/doctors',
+            // Inactive: 'https://harmony-backend-z69j.onrender.com/api/admin/get/inactive/doctors',
+            // TemporaryOff: 'https://harmony-backend-z69j.onrender.com/api/admin/get/temporaryoff/doctors',
+        };
+
+        try {
+            const response = await fetch(urlMap[role], {
+                method: "GET",
+                headers: { 'x-api-key': apiKey },
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed loading data");
+            }
+
+            const data = await response.json();
+            console.log("data", data)
+            setDoctorData(data?.staff?.doctors || data?.data?.approvalDoctors || data?.data?.pendingDoctors || data?.data?.rejectedDoctors || data?.data?.activeDoctors || data?.data?.InactiveDoctors || data?.data?.temporayOffDoctors || []);
+        } catch (error) {
+            console.error('Error fetching staff data:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // Handle data fetching based on selected filter
+    const roleSetter = (role: RoleType) => {
+        setSelected(role);
+        fetchDoctors(role);
+    };
+
+    useEffect(() => {
+        // Default data fetching on load (View All Doctors)
+        fetchDoctors('All');
+    }, []);
+
+    const handleMouseEnter = (buttonType: string) => {
+        setHoveredButton(buttonType);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredButton(null);
+    };
+
+    const getButtonStyle = (buttonType: string) => {
+        return hoveredButton === buttonType ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle;
+    };
+
+    if (loading) {
+        return (
+            <div className="d-flex justify-content-center align-items-center vh-100">
+                <Spinner animation="border" role="statusCard">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+                <h4 className="mx-2">Loading..</h4>
+            </div>
+        );
+    }
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px", padding: '20px 20px', width: "100%" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ display: "flex", marginRight: '10px', borderColor: '#2C297D' }}>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'Pending' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'Pending' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('Pending')}
+                        >
+                            Pendings
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'Rejected' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'Rejected' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('Rejected')}
+                        >
+                            Rejected
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'Approved' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'Approved' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('Approved')}
+                        >
+                            Approved
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'All' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'All' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('All')}
+                        >
+                            View All
+                        </Button>
+                        {/* <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'Active' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'Active' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('Active')}
+                        >
+                            Active
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'Inactive' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'Inactive' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('Inactive')}
+                        >
+                            Inactive
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className='mx-0'
+                            style={{
+                                backgroundColor: selected === 'TemporaryOff' ? '#2C297D' : '#FFFFFF',
+                                color: selected === 'TemporaryOff' ? '#FFFFFF' : '#2C297D',
+                            }}
+                            onClick={() => roleSetter('TemporaryOff')}
+                        >
+                            Temporary Off
+                        </Button> */}
+                    </div>
+                </div>
+            </div>
+
+            {doctorData.length === 0 ? (<div style={{ textAlign: 'center', fontSize: '1.5rem', color: '#888' }}>
+                No data found
+            </div>) : (<Grid container spacing={1}>
+                {doctorData.map((staff) => {
+                    const detailPath = `/consultants/${staff.id}`;
+                    return (
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={staff.id} style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Link href={detailPath} passHref>
+                                <div style={{ cursor: 'pointer' }}>
+                                    <StaffCard name={staff.name || staff.username} role={staff.role} imageUrl="/assets/avatar.jpg" />
+                                </div>
+                            </Link>
+                        </Grid>
+                    );
+                })}
+            </Grid>)}
+        </div>
+    );
+};
+
+export default StaffCardView;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,128 @@
 'use client'
 
+import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
-import { Button, Spinner } from "react-bootstrap";
+import { Button as BootstrapButton } from "react-bootstrap";
+import { RiAdminLine } from "react-icons/ri";
+import { GrUserManager } from "react-icons/gr";
+import { FaUserDoctor } from "react-icons/fa6";
 
+// Button Loading Component
+export function ButtonLoading() {
+  return (
+    <BootstrapButton disabled style={{ backgroundColor: '#fff', border: "1px dashed orange", width: "auto", color: "#ff6600", display: "flex", alignItems: "center" }}>
+      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      <span>Please wait...</span>
+    </BootstrapButton>
+  );
+}
 
 export default function Home() {
 
-  const [loading, setLoading] = useState(false);
+  const [loadingButton, setLoadingButton] = useState<string | null>(null);
+  const [hoveredButton, setHoveredButton] = useState<string | null>(null);
+
+  const handleClick = (buttonType: string) => {
+    setLoadingButton(buttonType);
+  };
+
+  const handleMouseEnter = (buttonType: string) => {
+    setHoveredButton(buttonType);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredButton(null);
+  };
+
+  const buttonStyle = {
+    background: "#fff",
+    border: "1px solid orange",
+    color: "#ff6600",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    transition: "background-color 0.3s, color 0.3s, border 0.3s",
+    width: "auto"
+  };
+
+  const buttonHoverStyle = {
+    background: "#ff6600",
+    border: "1px solid #ff6600",
+    color: "white"
+  };
+
+  const loadingButtonStyle = {
+    backgroundColor: "#f7f7f7",
+    borderColor: "#ccc",
+    color: "#999"
+  };
+
+  const getButtonStyle = (buttonType: string) => {
+    if (loadingButton === buttonType) {
+      return { ...buttonStyle, ...loadingButtonStyle };
+    } else if (hoveredButton === buttonType) {
+      return { ...buttonStyle, ...buttonHoverStyle };
+    }
+    return buttonStyle;
+  };
 
   return (
     <>
-      <div style={{width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <div style={{ width: "80%",display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
-          <Link href='/admin'><Button>{ loading ? <Spinner /> :'Go to Admin Page'}</Button></Link>
-          <Link href='/manager'><Button>{ loading ? <Spinner /> :'Go to manager Page'}</Button></Link>
-          <Link href='/creator'><Button>{ loading ? <Spinner /> :'Go to creator Page'}</Button></Link>
+      <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <div style={{ width: "80%", display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
+          <Link href='/admin' style={{ textDecoration: "none" }}> {loadingButton === 'Admin' ? (
+            <ButtonLoading />
+          ) : (
+            <BootstrapButton
+              variant="outline-warning"
+              onClick={() => handleClick("Admin")}
+              onMouseEnter={() => handleMouseEnter("Admin")}
+              onMouseLeave={handleMouseLeave}
+              style={getButtonStyle("Admin")}
+            >
+              Admin <RiAdminLine className="ml-2 fs-5" />
+            </BootstrapButton>
+          )}</Link>
+          <Link href='/manager' style={{ textDecoration: "none" }}> {loadingButton === 'manager' ? (
+            <ButtonLoading />
+          ) : (
+            <BootstrapButton
+              variant="outline-warning"
+              onClick={() => handleClick("manager")}
+              onMouseEnter={() => handleMouseEnter("manager")}
+              onMouseLeave={handleMouseLeave}
+              style={getButtonStyle("manager")}
+            >
+              Manager <GrUserManager className="ml-2 fs-5" />
+            </BootstrapButton>
+          )}</Link>
+          <Link href='/creator' style={{ textDecoration: "none" }}> {loadingButton === 'creator' ? (
+            <ButtonLoading />
+          ) : (
+            <BootstrapButton
+              variant="outline-warning"
+              onClick={() => handleClick("creator")}
+              onMouseEnter={() => handleMouseEnter("creator")}
+              onMouseLeave={handleMouseLeave}
+              style={getButtonStyle("creator")}
+            >
+              Creator <GrUserManager className="ml-2 fs-5" />
+            </BootstrapButton>
+          )}</Link>
+          <Link href='/consultants' style={{ textDecoration: "none" }}> {loadingButton === 'consultants' ? (
+            <ButtonLoading />
+          ) : (
+            <BootstrapButton
+              variant="outline-warning"
+              onClick={() => handleClick("consultants")}
+              onMouseEnter={() => handleMouseEnter("consultants")}
+              onMouseLeave={handleMouseLeave}
+              style={getButtonStyle("consultants")}
+            >
+              All Consultants <FaUserDoctor className="ml-2 fs-5" />
+            </BootstrapButton>
+          )}</Link>
         </div>
       </div>
     </>
